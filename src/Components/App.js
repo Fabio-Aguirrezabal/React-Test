ef157a07fd2b351d39e527833a7ebaa973e4f130
Fabio Aguirrezábal Pintos <fabio.aguirrezabal@outlook.com>
2023-04-03 11:28:41 +0200

    Tutorial testimonios fixed the bold in between text

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import logo from '../Resources/logo.svg';
 import './App.css';
 import Testimonio from './Testimonio';
@@ -12,26 +13,27 @@ function App() {
         pais='Singapore'
         cargo='Software Engineer'
         empresa='Amazon'
-        testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        testimonio={<Fragment>It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. <strong>freeCodeCamp changed my life.</strong></Fragment>}
         imagen='Shawn'
       />
-      <Testimonio 
-        nombre='Emma Bostian'
-        pais='Sweden'
-        cargo='Software Engineer'
-        empresa='Spotify'
-        testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
-        imagen='Emma'
-      />
+     
 <Testimonio 
         nombre='Sarah Chima'
         pais='Nigeria'
         cargo='Software Engineer'
         empresa='ChatDesk'
-        testimonio="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+        testimonio={<Fragment><strong>freeCodeCamp was the gateway to my career</strong> as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.</Fragment>}
         imagen='Sarah'
       />
 
+ <Testimonio 
+        nombre='Emma Bostian'
+        pais='Sweden'
+        cargo='Software Engineer'
+        empresa='Spotify'
+        testimonio={<Fragment>I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on <strong>freeCodeCamp gave me the skills</strong> and confidence I needed to land my dream job as a software engineer at Spotify.</Fragment>}
+        imagen='Emma'
+      />
       </div>
     </div>
     
